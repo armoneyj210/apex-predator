9570085466777731caaafda2b61fda7580940a25
Joshua Armoney <armoneyj210@gmail.com>
2020-02-07 11:54:10 -0500

    models

--- apex/models.py
+++ apex/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Map(models.Model):
+    name = models.CharField(max_length=30)
+    map_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class WeaponType(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=30)
+    weapon_type = models.CharField(max_length=30)
+    ammo = models.CharField(max_length=20)
+    image_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=20)
+    passive_ability = models.CharField(max_length=30)
+    ability = models.CharField(max_length=30)
+    super = models.CharField(max_length=30)
+    image_url = models.TextField()
+    background = models.TextField()
+    skin = models.CharField(max_length=200)
+    kills = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Skin(models.Model):
+    name = models.CharField(max_length=30)
+    image_url = models.TextField()
+
+    def __str__(self):
+        return self.name
