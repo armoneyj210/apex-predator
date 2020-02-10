09bdd4cf9381f9a88c19666898a4604d3b4ad8fc
Joshua Armoney <armoneyj210@gmail.com>
2020-02-09 20:25:38 -0500

    home.jsx sperated and styled

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
-
+import video from "../Home/mylivewallpapers.com-Apex-Legends-Banner.mp4";
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <div>
+          <video autoPlay loop>
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
         <div className="home-container">
           <div>
             <h1 className="home-header">
               Do you want to become an Apex Legend?
             </h1>
+            <h1 className="home-header2">Don't know where to begin?</h1>
+            <h1 className="home-header3">Let us help you get started!</h1>
           </div>
 
           <div>
             <a href="/character">
-              <button className="home-cta">Get Started</button>
+              <button className="home-cta">Become a Legend!</button>
             </a>
           </div>
         </div>
