9ff6ab59bbb44bca9973bde827d8bdecafd7649b
Joshua Armoney <armoneyj210@gmail.com>
2020-02-14 14:34:20 -0500

    digital ocean start

--- apex_project/settings.py
+++ apex_project/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static')
+]
 
 
 # Quick-start development settings - unsuitable for production
--- apex_project/urls.py
+++ apex_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apex.urls'))
+     re_path(r'^.*$', views.FrontendAppView.as_view()),
 ]
