72f1184ba255622cf5b04a3e80c3b3526b6a0619
Joshua Armoney <armoneyj210@gmail.com>
2020-02-14 14:39:45 -0500

    digi

--- apex_project/urls.py
+++ apex_project/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.FrontendAppView.as_view()),
     path('api/v1/', include('apex.urls'))
-     re_path(r'^.*$', views.FrontendAppView.as_view()),
+    re_path(r'^.*$', views.FrontendAppView.as_view()),
 ]
